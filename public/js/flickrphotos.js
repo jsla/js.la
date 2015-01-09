2dbf68aa46cc305ce375fb10f766321dd040d4e2
Jacob Lowe <jacoblowe2.0@gmail.com>
2015-01-08 17:48:51 -0800

    remix

--- public/js/flickrphotos.js
+++ public/js/flickrphotos.js
@@ -1,29 +1,38 @@
 
-exports.addPhotos = function( ) {
+// this gets called to find all elements with data-flickr
+exports.addPhotos = function( callback ) {
     // execute after page has loaded
-
+    callback = callback || function noop() {};
     var els = document.querySelectorAll( '[data-flickr]' ),
         urls;
 
     // dont execute if not flickr els are found
     if ( !els.length ) {
+
+        callback( new Error( 'No [data-flickr] targets found' ), true );
+
+
         return;
     }
 
     els = Array.prototype.slice.call( els, 0 ); //normal arrays are better
 
+    // makes an array of urls
     urls = els.map( getUrls );
-    getJSONP( urls );
+    getJSONP( urls, callback );
+
 };
 
 function getUrls( el ) {
     return el.getAttribute( 'data-flickr' ); // this should have the url in it
 }
 
-function createScriptEl( url ) {
+var createScriptEl =
+module.exports.createScriptEl = function ( url, callback ) {
     var script = document.createElement( 'script' ),
         id = '_flickr' + ( Math.floor( Math.random() * 5000 ) );
-    window[ id ] = handleCallback.bind( null, id, url ); // this is the callback for the api
+    window[ id ] = typeof callback === 'function' ? 
+        callback.bind( null, id, url ) : handleCallback.bind( null, id, url ); // this is the callback for the api
     script.src = url + '&jsoncallback=' + id;
     return script; 
 }
@@ -33,13 +42,18 @@ function appendEl( el, parent ) {
     parent.appendChild( el );
 }
 
-function getJSONP( urls ) {
+function getJSONP( urls, callback ) {
+    // maps a bunch of script tags { DOM node Object } into an array
+    // each on links to a JSONP endpoint with a unique id
     var els = urls.map( createScriptEl ); 
+    // appends script tags
     els.forEach( appendEl );
+    callback( null, true );
 }
 
 function handleCallback( id, url, data ){
     delete window[ id ]; // cleanup
+    // finds the data-flickr el, and appends data to it
     addToPage( data, document.querySelector( '[data-flickr="' + url + '"]') );
 }
 
@@ -49,25 +63,23 @@ function appendPhoto( el, photo ) {
         img = document.createElement( 'img' );
 
     link.rel = 'lightbox';
-    link.href = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "z.jpg";
-    img.alt = photo.title;
-    img.src = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "t.jpg",
-
-    appendEl( img, link );
+    link.href = getPhotoLink( photo );
+    link.setAttribute( 'style', 'background-image: url(' + getPhotoSRC( photo ) + ')' )
     appendEl( link, el );
 
 }
 
+var getPhotoLink =
+module.exports.getPhotoLink = function( photo ) {
+    return "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "z.jpg";
+}
+
+var getPhotoSRC =
+module.exports.getPhotoSRC = function( photo ) {
+    return "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "t.jpg";
+}
+
 function addToPage( resp, el ) {
-    var header = document.createElement( 'h1' ),
-        center = document.createElement( 'center' ),
-        br = document.createElement( 'br' );
-
-    center.innerText = "Photos";
-    // adding header to dom
-    appendEl( center, header );
-    appendEl( header, el );
-    appendEl( br, el );
-
+    // each photo
     resp.photos.photo.forEach( appendPhoto.bind( null, el ) );
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,48 @@
 
 
 var $ = window.jQuery = require( 'jquery' ),
+<<<<<<< HEAD
+    merge = require( 'merge' ),
+    flickr = require( './flickrphotos' ),
+    hero = require( './components/hero' ),
+    nav = require( './components/nav' );
+=======
     flickr = require( './flickrphotos' );
+>>>>>>> master
 
 // monkey patch jquery plugins
 require( './jquery.fancybox.pack.js' );
 require( './jquery.fancybox-media.js' );
 
 $(function() {
+<<<<<<< HEAD
+    var $body = $( 'body' ),
+        fancyboxOptions = {
+            openEffect  : 'none',
+            closeEffect : 'none',
+            beforeShow: function(){
+                $body.css({ overflowY: 'hidden' })
+            },
+            afterClose: function(){
+                $body.css({ overflowY: 'visible' })
+            },
+            helpers : {
+                media : {}
+            }
+        };
+
+    // attach components
+    nav.attach( '.jsla-mobile-menu' );
+    hero.attach( '.jsla-hero' );
+
+    flickr.addPhotos(); // this starts the script
+    $( 'a[rel=lightbox-video]' ).fancybox( merge( {}, fancyboxOptions, { 
+        showNavArrows: false 
+    } ) );
+    $('a[rel=lightbox]').fancybox( fancyboxOptions );
+
+});
+=======
 
     flickr.addPhotos(); // this starts the script
 
@@ -33,3 +68,4 @@ $(function() {
 
     $('a[rel=lightbox]').fancybox()
 });
+>>>>>>> master
