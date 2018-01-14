0dc3c561e25c4a581044af01f78ecb4563e934a1
David Guttman <david@davidguttman.com>
2018-01-14 10:48:23 -0800

    feat(scripts): use dotenv and env vars for authentic creds

--- script/fetch-admin.js
+++ script/fetch-admin.js
@@ -1,13 +1,14 @@
+require('dotenv').config()
+
 var map = require('map-async')
 var Authentic = require('authentic-client')
 
-var creds = require('./creds.json')
+var creds = {
+  email: process.env.AUTHENTIC_EMAIL,
+  password: process.env.AUTHENTIC_PASSWORD
+}
 
-var auth = Authentic({
-  server: 'https://authentic.apps.js.la',
-  email: creds.email,
-  password: creds.password
-})
+var auth = Authentic({ server: 'https://authentic.apps.js.la' })
 
 var urls = {
   speakers: 'https://admin.apps.js.la/api/list/speaker',
