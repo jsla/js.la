eb3919ea65633894e92c2c04d1285c932edb6610
David Guttman <david@davidguttman.com>
2018-01-30 21:48:11 -0800

    feat: update-data script to populate 2018

--- script/update-site-data.js
+++ script/update-site-data.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
 var path = require('path')
 
-var siteData = require('../public/_data.json')
+var pastData = require('../public/events/2018/_data.json')
+var currentData = require('../public/_data.json')
 var fetchAdmin = require('./fetch-admin')
 var extractShow = require('./extract-show')
 
@@ -11,13 +12,32 @@ fetchAdmin(function (err, everything) {
     return process.exit(1)
   }
 
+  updateCurrent(everything)
+  updatePrevious(everything)
+})
+
+function updateCurrent (everything) {
   var show = extractShow(everything)
-  siteData.current.date = show.date
-  siteData.current.datetime = show.datetime
-  siteData.current.host = show.host
-  siteData.current.speakers = show.speakers
-  siteData.current.sponsors = show.sponsors
+  currentData.current.date = show.date
+  currentData.current.datetime = show.datetime
+  currentData.current.host = show.host
+  currentData.current.speakers = show.speakers
+  currentData.current.sponsors = show.sponsors
 
   var target = path.join(__dirname, '../public/_data.json')
-  fs.writeFileSync(target, JSON.stringify(siteData, null, 4))
-})
+  fs.writeFileSync(target, JSON.stringify(currentData, null, 4))
+}
+
+function updatePrevious (everything) {
+  var prevShows = []
+
+  var t0 = new Date('2018-01-01T00:00:00.000Z').valueOf()
+  var msMonth = 31 * 24 * 3600 * 1000
+  for (var i = t0; i < Date.now(); i += msMonth) {
+    prevShows.push(extractShow(everything, i))
+  }
+
+  pastData.events = prevShows
+  var target = path.join(__dirname, '../public/events/2018/_data.json')
+  fs.writeFileSync(target, JSON.stringify(pastData, null, 4))
+}
