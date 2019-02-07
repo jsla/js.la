81c007869aa70d7ad568a32ef522566c36e05c0d
Georgi Yovchev <georgi.yovchev@sciant.com>
2019-02-07 22:48:04 +0200

    feat: added design ideas for nodeschool and lunchjs

---
+++ public/assets/js/custom.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+    margin:10,
+    nav: true,
+    loop: true,
+    dots: false,
+    //navText: ["<img src='/images/js.png' width='30' >","<img src='/images/js.png' width='30'>"],
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    items: 1
+})
